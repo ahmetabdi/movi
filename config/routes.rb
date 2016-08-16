9d51b7595bd1de440e51d9bcbe99119315b659b9
Ahmet <ahmetabdi@gmail.com>
2016-08-16 21:31:44 +0100

    Design

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+
+  get 'movies', to: 'pages#movies'
+  get 'ratings', to: 'pages#ratings'
+  get 'tv-shows', to: 'pages#tv_shows'
+  get 'trending', to: 'pages#trending'
 end
