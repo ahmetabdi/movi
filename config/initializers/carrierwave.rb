c15e10578709bb5a5b56c1ed9f1c539817269a9a
Ahmet Abdi <ahmetabdi@gmail.com>
2016-08-22 09:50:07 +0100

    Update config for carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,47 @@
 require 'carrierwave/orm/activerecord'
 
 unless Rails.env.development?
+  # CarrierWave.configure do |config|
+  #   config.storage = :aws
+  #   config.fog_credentials = {
+  #     provider:              'AWS',
+  #     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+  #     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  #     region:                ENV['AWS_REGION']
+  #   }
+  #   config.fog_directory  = ENV['S3_BUCKET_NAME']
+  #   config.fog_public     = false
+  #   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+  # end
   CarrierWave.configure do |config|
-    config.storage = :aws
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION']
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+    config.aws_acl    = 'public-read'
+
+    # Optionally define an asset host for configurations that are fronted by a
+    # content host, such as CloudFront.
+    # config.asset_host = 'http://example.com'
+
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+    # Set custom options such as cache control to leverage browser caching
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=604800'
     }
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
-    config.fog_public     = false
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_REGION') # Required
+    }
+
+    # Optional: Signing of download urls, e.g. for serving private content through
+    # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
+    # configured:
+    # config.aws_signer = -> (unsigned_url, options) do
+    #   Aws::CF::Signer.sign_url(unsigned_url, options)
+    # end
   end
 end
